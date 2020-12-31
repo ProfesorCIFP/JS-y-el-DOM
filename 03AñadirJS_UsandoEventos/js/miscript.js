ce20121ab846bee226ada5ec4939fee2d5f9132d
ProfesorCIFP <cjgardia.cifp@gmail.com>
2020-12-31 17:20:44 +0000

    Tercera Actividad: Load vs DOMContentLoader

---
+++ 03AñadirJS_UsandoEventos/js/miscript.js
@@ -0,0 +1,22 @@
+function inicializar() {
+    document.getElementById('parrafo1').style.color = 'red';
+    document.getElementById('parrafo2').style.color = 'blue';
+}
+
+function inicializarFecha() {
+    var fechaHoy = new Date();
+    var dia = fechaHoy.getDate();  // Obtenemos el día de la fecha actual.
+    var mes = fechaHoy.getMonth();  // Obtenemos el mes de la fecha actual.
+    var agno = fechaHoy.getFullYear();  // Obtenemos el año de la fecha actual.
+
+    document.getElementById('dia').value = dia;
+    document.getElementById('mes').value = mes + 1;
+    document.getElementById('ano').value = agno;
+}
+// El evento "load" se dispara el evento cuando la página 
+//  se ha terminado de cargar completamente (imágenes, scripts, etc.)
+window.addEventListener("load", inicializar);
+
+// El evento "DOMContentLoaded" se dispara cuando el DOM ya se halla creado, 
+//  no espera a la carga completa de la página.
+document.addEventListener("DOMContentLoaded", inicializarFecha);
